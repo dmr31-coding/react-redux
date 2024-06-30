5769bc8e68f3a846e4ca436b53ebf85c4013d73c
Dinesh Magar <magard1010@gmail.com>
2024-06-30 18:19:51 +0545

    redux toolkit redux store slice useSelector useDispatch theme Reducer

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,24 @@
+import { useSelector, useDispatch } from "react-redux"
+import { decrement, increment, incrementByAmount } from "./counterSlice"
+
+function Counter() {
+  const count = useSelector((state) => state.counter.count)
+  const themeTextColor = useSelector((state) => state.theme.color)
+
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+        <button className='button' aria-label='Increment value' onClick={() => { dispatch(increment()) }}> + </button>
+
+<span className='value' style={{ color: themeTextColor }}>Count: {count}</span>
+
+<button className='button' aria-label='Decrement value' onClick={() => { dispatch(decrement()) }}> - </button>
+
+<button className='button' onClick={() => { dispatch(incrementByAmount(20)) }}> Increment by Amount </button>
+
+    </div>
+  )
+}
+
+export default Counter
